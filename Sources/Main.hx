4dceae48c0a36e4d089217f0a6bee86e3d4021c6
smoothie-ws <kibot.email@gmail.com>
2024-12-13 15:49:31 +1000

    Update submodules

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,56 +1,93 @@
-package;
-
-import kha.Assets;
+import kha.Image;
 import kha.Color;
-import kha.Framebuffer;
-import kha.Scheduler;
 import kha.System;
+import kha.Assets;
+import kha.math.FastVector2;
+// s2d
+import s2d.S2D;
+import s2d.objects.Sprite;
+import s2d.objects.PointLight;
 
-class Main {
-	static var logo = ["1 1 1 1 111", "11  111 111", "1 1 1 1 1 1"];
+inline function pickClosest(x:Float, y:Float, sprites:Array<Sprite>):Sprite {
+	var minDist = Math.POSITIVE_INFINITY;
+	var closest = null;
+	for (sprite in sprites) {
+		var v = sprite.vertices;
+		var cp:FastVector2 = {
+			x: (v[0].x + v[1].x + v[2].x + v[3].x) / 4,
+			y: (v[0].y + v[1].y + v[2].y + v[3].y) / 4
+		};
 
-	static function update(): Void {
+		var dist = Math.sqrt((x - cp.x) * (x - cp.x) + (y - cp.y) * (y - cp.y));
+		if (0.5 > dist && dist < minDist) {
+			closest = sprite;
+			minDist = dist;
+		}
 	}
 
-	static function render(frames: Array<Framebuffer>): Void {
-		// As we are using only 1 window, grab the first framebuffer
-		final fb = frames[0];
-		// Now get the `g2` graphics object so we can draw
-		final g2 = fb.g2;
-		// Start drawing, and clear the framebuffer to `petrol`
-		g2.begin(true, Color.fromBytes(0, 95, 106));
-		// Offset all following drawing operations from the top-left a bit
-		g2.pushTranslation(64, 64);
-		// Fill the following rects with red
-		g2.color = Color.Red;
+	return closest;
+}
 
-		// Loop over the logo (Array<String>) and draw a rect for each "1"
-		for (rowIndex in 0...logo.length) {
-		  final row = logo[rowIndex];
+class App extends sui.App {
+	override inline function setup() {
+		var stage = new S2D(scene);
+		stage.anchors.fill(scene);
 
-		  for (colIndex in 0...row.length) {
-		    switch row.charAt(colIndex) {
-		      case "1": g2.fillRect(colIndex * 16, rowIndex * 16, 16, 16);
-		      case _:
-		    }
-		  }
+		var sprites = [];
+		for (_ in 0...4) {
+			var sprite = new Sprite(stage);
+			sprite.blendMode = AlphaClip;
+			sprite.vertices = [{x: -1, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 0, y: -1}];
+
+			sprite.diffuseMap = Assets.images.get('diffuse');
+			sprite.normalMap = Assets.images.get('normal');
+			sprite.ormMap = Assets.images.get('orm');
+			sprite.emissionColor = Color.fromFloats(0.025, 0.025, 0.025);
+
+			sprites.push(sprite);
 		}
 
-		// Pop the pushed translation so it will not accumulate over multiple frames
-		g2.popTransformation();
-		// Finish the drawing operations
-		g2.end();
-	}
+		var light = new PointLight(stage);
+		light.z = 0.1;
 
-	public static function main() {
-		System.start({title: "Project", width: 1024, height: 768}, function (_) {
-			// Just loading everything is ok for small projects
-			Assets.loadEverything(function () {
-				// Avoid passing update/render directly,
-				// so replacing them via code injection works
-				Scheduler.addTimeTask(function () { update(); }, 0, 1 / 60);
-				System.notifyOnFrames(function (frames) { render(frames); });
-			});
+		var m = new MouseArea(scene);
+		m.anchors.fill(scene);
+
+		var moving = false;
+		var movingSprite = sprites[0];
+
+		m.notifyOnMove(function(x, y, mx, my) {
+			if (!moving) {
+				var mv = pickClosest(x / m.width * 2 - 1, y / m.height * 2 - 1, sprites);
+				if (mv != movingSprite)
+					movingSprite = mv;
+			} else if (movingSprite != null)
+				movingSprite.translate(mx / m.width * 2, my / m.height * 2);
 		});
+		m.notifyOnDown(function(button, x, y) {
+			if (button == 0) {
+				moving = true;
+				if (movingSprite != null)
+					movingSprite.scale(0.9, 0.9);
+			} else if (button == 1)
+				if (movingSprite != null)
+					movingSprite.scale(1 / 0.9, 1 / 0.9);
+		});
+		m.notifyOnUp(function(button, x, y) {
+			if (button == 0)
+				moving = false;
+		});
+
+		SUI.notifyOnUpdate(function() {
+			light.x = Math.sin(System.time) * 0.5 + 0.5;
+			light.y = Math.cos(System.time) * 0.5 + 0.5;
+		});
+	}
+}
+
+class Main {
+	public static function main() {
+		var app = new App(756, 756);
+		SUI.start(app);
 	}
 }
