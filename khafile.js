40c97b716309a9436a98b62a910098f586e06134
smoothie <kibot.email@gmail.com>
2025-01-04 16:26:54 +1000

    Updatae `khafile.js`

--- khafile.js
+++ khafile.js
@@ -3,9 +3,9 @@ let project = new Project("sEngine");
 project.addAssets("assets/**");
 project.addSources("src");
 
-await project.addProject("Subprojects/sCore");
-await project.addProject("Subprojects/sUI");
-await project.addProject("Subprojects/s2D");
+await project.addProject("subprojects/sCore");
+await project.addProject("subprojects/sUI");
+await project.addProject("subprojects/s2D");
 
 // Available Compiler Flags
 // SENGINE_DEBUG_FPS -> enables FPS debugging
